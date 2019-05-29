4d034db574b27aaed7f844a8f0a7fffb719a6f50
Arsenije Savic <arsenijesavic@me.com>
2019-05-29 12:13:22 +0200

    ...

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'lie and and theft as practice',
+    author: 'Arsenije Savic',
+    description: `https://lieandtheftaspractice.art/`,
+    homepage: `https://lieandtheftaspractice.art/`,
+    keywords: 'art',
   },
+
   plugins: [
     'gatsby-plugin-react-helmet',
     {
